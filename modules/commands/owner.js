a4ac1b000e5746db3970fc59662cc2381b1d45aa
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-03-14 21:00:27 +0800

    Update owner.js

--- modules/commands/owner.js
+++ modules/commands/owner.js
@@ -1,15 +1,28 @@
 module.exports.config = {
-  name: "owner", author: "Sethdico", version: "1.2", category: "Utility", description: "Contact the owner", adminOnly: false, usePrefix: false, cooldown: 5,
+  name: "owner",
+  author: "sethdico",
+  version: "2.2",
+  category: "Utility",
+  description: "the dev",
+  adminOnly: false,
+  usePrefix: false,
+  cooldown: 5,
 };
 
-module.exports.run = async function ({ event, api, reply }) {
+module.exports.run = async function ({ event, api }) {
+  const id = event.sender.id;
+  const gif = "https://media1.tenor.com/m/Mq6ZeawKT1MAAAAd/nazo-no-kanojo-x-nazo-no-kanojo.gif";
+  const msg = "i'm seth asher and i made this bot. check my links if u want to talk.";
+
   const buttons = [
-    { type: "web_url", url: "https://www.facebook.com/seth09asher", title: "Add Seth" },
-    { type: "web_url", url: "https://github.com/sethdico", title: "GitHub" }
+    { type: "web_url", url: "https://www.facebook.com/seth09asher", title: "facebook" },
+    { type: "web_url", url: "https://github.com/sethdico", title: "github" }
   ];
+
   try {
-      await api.sendButton("👑 **Bot Owner**\nCreated by Seth Asher Salinguhay.", buttons, event.sender.id);
+    await api.sendAttachment("image", gif, id);
+    await api.sendButton(msg, buttons, id);
   } catch (e) {
-      reply("👑 **Owner:** Seth Asher Salinguhay\nFB: facebook.com/seth09asher");
+    api.sendMessage("seth asher salinguhay\nfb: seth09asher\ngithub: sethdico", id);
   }
 };
