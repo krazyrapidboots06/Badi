7653f4a90e97167081f39e34d31bfa1c3462b30e
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-03-14 22:16:49 +0800

    Update uid.js

--- modules/commands/uid.js
+++ modules/commands/uid.js
@@ -1,25 +1,27 @@
-const axios = require("axios");
-
 module.exports.config = {
     name: "uid",
     author: "sethdico",
     category: "Utility",
+    description: "see your user profile card",
     adminOnly: false,
-    usePrefix: false
+    usePrefix: false,
+    cooldown: 5
 };
 
-module.exports.run = async function ({ event, args, api, reply }) {
-    const targetID = event.message?.reply_to?.sender_id || args[0] || event.sender.id;
-
+module.exports.run = async function ({ event, api, reply }) {
+    const id = event.sender.id;
+    const pic = `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=${global.PAGE_ACCESS_TOKEN}`;
+    
     try {
-        const info = await api.getUserInfo(targetID);
-        const pic = `https://graph.facebook.com/${targetID}/picture?width=512&height=512&access_token=${global.PAGE_ACCESS_TOKEN}`;
-        
-        const msg = `name: ${info.name}\nid: ${targetID}`;
+        const info = await api.getUserInfo(id);
+        const msg = `👤 **user profile**\n\nname: ${info.name}\nid: ${id}\n\nstatus: active`;
 
-        await api.sendAttachment("image", pic, event.sender.id);
-        reply(msg.toLowerCase());
+        await api.sendAttachment("image", pic, id);
+        
+        const buttons = [{ type: "postback", title: "copy id", payload: "copy_id" }];
+        await api.sendButton(msg.toLowerCase(), buttons, id);
+        
     } catch (e) {
-        reply(`id: ${targetID}`);
+        reply(`id: ${id}`);
     }
 };
