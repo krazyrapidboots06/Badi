6255e318bdbbb5e2e49dfb994ac25fa34f189c90
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-03-14 21:30:31 +0800

    Update uid.js

--- modules/commands/uid.js
+++ modules/commands/uid.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 module.exports.config = {
     name: "uid",
     author: "sethdico",
@@ -7,15 +9,15 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ event, args, api, reply }) {
-    const targetID = args[0] || event.sender.id;
+    const targetID = event.message?.reply_to?.sender_id || args[0] || event.sender.id;
 
     try {
         const info = await api.getUserInfo(targetID);
+        const pic = `https://graph.facebook.com/${targetID}/picture?width=512&height=512&access_token=${global.PAGE_ACCESS_TOKEN}`;
         
-        const msg = ` user info\n\n` +
-                    `name: ${info.name}\n` +
-                    `psid: ${targetID}`;
+        const msg = `name: ${info.name}\nid: ${targetID}`;
 
+        await api.sendAttachment("image", pic, event.sender.id);
         reply(msg.toLowerCase());
     } catch (e) {
         reply(`id: ${targetID}`);
