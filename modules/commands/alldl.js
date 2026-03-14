18d246b78350f90a3b964723a4279a9594d2d599
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-03-14 21:50:23 +0800

    Update alldl.js

--- modules/commands/alldl.js
+++ modules/commands/alldl.js
@@ -2,19 +2,26 @@ const axios = require("axios");
 
 module.exports.config = {
     name: "alldl",
-    aliases: ["dl", "download"],
-    author: "Sethdico",
-    version: "1.0",
+    author: "sethdico",
+    version: "1.3",
     category: "Media",
-    description: "All in One Downloader. Download videos from YouTube, Facebook, Instagram, TikTok, and Twitter/X.",
     adminOnly: false,
     usePrefix: false,
-    cooldown: 10,
+    cooldown: 5,
 };
 
 module.exports.run = async function ({ event, args, api, reply }) {
-    const url = args[0];
-    if (!url) return reply("send a link.");
+    let raw = event.message?.reply_to?.text || args.join("");
+    
+    if (!raw) {
+        return reply("📥 **all in one downloader**\ngrabs vids from tiktok, fb, ig, youtube, x, etc.\n\nusage:\n- alldl <link>\n- or just reply to a link with 'alldl'\n\nfb lite blocking ur link? just add spaces or use [dot] like this:\nalldl https://www . tiktok [dot] com / video");
+    }
+
+    let url = raw.replace(/\s+/g, "").replace(/\[dot\]|\(dot\)/gi, ".");
+
+    if (!url.includes("http")) {
+        return reply("that doesn't look like a valid link.");
+    }
 
     if (api.sendTypingIndicator) api.sendTypingIndicator(true, event.sender.id);
 
@@ -23,16 +30,15 @@ module.exports.run = async function ({ event, args, api, reply }) {
             params: { url: url }
         });
 
-        const videoUrl = res.data.data.videoUrl;
-        const platform = res.data.data.platform;
+        const vid = res.data.data?.videoUrl;
 
-        if (videoUrl) {
-            await api.sendAttachment("video", videoUrl, event.sender.id);
+        if (vid) {
+            await api.sendAttachment("video", vid, event.sender.id);
         } else {
-            reply("couldn't get the video link.");
+            reply("couldn't grab the video.");
         }
     } catch (e) {
-        reply("download failed. link might be private or broken.");
+        reply("failed. link is either private or broken.");
     } finally {
         if (api.sendTypingIndicator) api.sendTypingIndicator(false, event.sender.id);
     }
