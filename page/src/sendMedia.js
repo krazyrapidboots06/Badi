03c3efe7818fa32d571d4dca97ab2c8754c59ac4
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-03-30 07:49:44 +0800

    Refactor sendMedia to use apiQueue for requests

--- page/src/sendMedia.js
+++ page/src/sendMedia.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
+const API_VERSION = process.env.API_VERSION || "v21.0";
 
 module.exports = function (event) {
-  return async function sendMedia(url, mediaType, buttons = [], senderID) {
+  return async function sendMedia(url, mediaType, buttons =[], senderID) {
     const recipientID = senderID || event.sender.id;
 
     const formattedButtons = buttons.slice(0, 3).map(btn => {
@@ -9,15 +10,15 @@ module.exports = function (event) {
       return { type: "postback", title: btn.title, payload: btn.payload || btn.title.toUpperCase() };
     });
 
-    try {
-      await axios.post(`https://graph.facebook.com/v21.0/me/messages?access_token=${global.PAGE_ACCESS_TOKEN}`, {
+    global.apiQueue.add(() =>
+      axios.post(`https://graph.facebook.com/${API_VERSION}/me/messages?access_token=${global.PAGE_ACCESS_TOKEN}`, {
         recipient: { id: recipientID },
         message: {
           attachment: {
             type: "template",
             payload: {
               template_type: "media",
-              elements: [
+              elements:[
                 {
                   media_type: mediaType,
                   url: url,
@@ -27,9 +28,7 @@ module.exports = function (event) {
             }
           }
         }
-      });
-    } catch (e) {
-      console.error("❌ sendMedia fail:", e.response?.data || e.message);
-    }
+      })
+    ).catch(e => console.error("sendMedia fail:", e.response?.data || e.message));
   };
 };
